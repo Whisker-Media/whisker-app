b7c0b8cefe2a105f4bbb01afccd0f94b84a0f5fe
polylogue <evanodoch@gmail.com>
2025-12-13 16:21:23 +0000

    Absolutely awful landing page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,15 +1,23 @@
-import './Landing.css'
+import React from 'react';
+import Header from './Header.jsx';
+import './Landing.css';
 
 function Landing() {
   return (
-    <div className="landing">
-      <header className="landing-header">
-        <h1>Welcome to Whisker</h1>
-        <p>A Scratch-themed social media platform</p>
-        <a href="/app" className="enter-button">Jump in</a>
-      </header>
+    <div className="landing-root">
+      <div className="landing">
+        <Header />
+        <main className="landing-content">
+          <div className="content-wrapper">
+            <h1>Welcome to Whisker</h1>
+            <p>A Scratch-themed social media platform</p>
+            <a href="/app" className="enter-button">Jump in</a>
+          </div>
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Landing
+
+export default Landing;
