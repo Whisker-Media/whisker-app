db3131ad53f10e9e283b25d472cba0da2be3f607
Mase <masonkember1+github@gmail.com>
2025-12-14 15:39:10 +0000

    Organise the files Fixes #5

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,9 +1,13 @@
+/* Usual Imports */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+/* Page Imports */
 import Landing from './Landing.jsx'
-import App from './App.jsx'
-import './index.css'
+import App from '../App.jsx'
+
+/* Style Imports */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
