9bece4424098840ba7e0cd970828dc7fb380a451
kRxZy_kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-22 14:59:19 +0000

    remove private key

--- src/lib/firebase-admin/firebaseAdminConfig.js
+++ src/lib/firebase-admin/firebaseAdminConfig.js
@@ -5,10 +5,10 @@ dotenv.config();
 const serviceAccountKey = {
   "type": "service_account",
   "project_id": "whisker-media",
-  "private_key_id": "005ebd798a32eaf3c4745851641e7e377e3d7168",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC2dqc/7IVTLuKx\n8kUOZlQLGf9N7eVu6HB7IG4fa3VgTZKmhB/ihTJmy4epxyFWZEdeO6qj+sGgNhHL\nHF/0alevNIsgKi+KHrsSDHrhuyf5+1J2ZiQv8d1IC9OiwKeZHPAWWEQEoY4khsXO\nMSJERKT2MMM2U8LSAAdjVY230YlrV4w2wu5NblVpRbW1WQKAmInTP9F0EEKUD1mb\nUmUMhIHDlpU5EAdunR9HigY6hDKNitNnzCvQPQS02pIDfkfmRQeeA5D+tqFrSVdA\nJvkxY7aHby/QEUyr7u+Zy7ilgY9ohTmg65k+TtpSycEXY9dzJBXQzRJkUe9hM3uv\nTFkJD61hAgMBAAECggEAB8BCmpSRlVcIikagaFwwdchE8CqK9IsrPA/EndESy2SA\nNlKmiBjgaz16JdwvrlvULEyckxqz3vptBxPNf7SExQyWXGduXY+worOlBLHOoO/s\nMNtl7iN4IvddpixI8lgoWsqlGQ1D3IldHcU4j23bFBo+MLCeNg2KQs3/ly9vhv++\nqdLn4gQ79WQ9z6IagSrUkNZh6WVJIcHOdSBQFXUquDyQf+VTzFKL89KIMentbUXR\n7KtLR6Z7b8ariy5Ib/RLUf3ga+Sj3aTv4CTCbrGIIspxm1xc3kK6JucHdYpv2AOn\nCY8Tsd/0wree3PDaFIwNumn6GsuPjG1ugq0a3cw5gQKBgQDpyTxCq7hsuxbHlUJL\nCE44yrx3bdXo3KAhQ9Qv+R88t1zXxQh7bsreM4zeL9haHP+f4ITdzgpZ+PtHwaC2\nRCuBAdXz3JZn+gkFJ+QIwmvM3yfwM05iqj4RNUkFg7HDHFPMiLSWsm5XpS0uW9Q6\nJV6FrNyH0SOZgD9Au1k39nu0GwKBgQDHzQOSKuUs8dfUwe+IFrI/xsVORk0+lg+R\nc49iJ4KtvwzMfcripvZGYKJN4sCA+RKhpPWnfCac6sJLjvBc1GxZ5OVMyWJ0FTkx\nQoB+1B8CRXdlVNsrPJQkILlJhDZKmdk8zJ5AQIG9SG1Y7xTambakJLPN0HQBSDhf\nuYCYtrUkMwKBgQDTVXkm36pQh/M4qeGKiFCFoCcsYtqKrN4IFBO85htpvGuuFUvs\noKO8p6cAtwmBtFMTyFbzVq0e1ib4n71S5l7ZOLqavl4sUedThNLrZC0kvtmQliCW\nqkR/M8b1uC26LWJHM5Dj5RpAGm20SYiYo21Fs5SLJfBjc346WuF06Fi83wKBgE3n\nGNpYEU1p6n6RM2T7fHqzdODlNl8NwXoz9ZcO0rwN/t6IuZ+P+/xK113I6Mgg1Wvj\nVsj79k4+lo1BaWS4ipquTd2Xs+SRFeCoDlPlP0XycfA3mtEL6LMBc10/yK9jtzkm\nixmATVXRNJl1dssZHgZ4LQc1nNWXJCvmt0ZUDuLTAoGBAJXsqYNkL/hZcPsXoBGd\nS2bE6WGZ++JgmVK9rz4Cy3uvtEzL+UwnzvSMCyBLaBJ4VFflAIBmSd/9qrh34kU/\nq+Iz0lePjd2CJLKEobOozI91e5UQxss48OH0JtvLMHfgUbUaplcyrV+pOQ3c2FfY\nvFgVo5xXdBQMQ94QJTk7eStR\n-----END PRIVATE KEY-----\n",
+  "private_key_id": "REMOVED_FOR_SECURITY_REASONS",
+  "private_key": "REMOVED_FOR_SECURITY_REASONS",
   "client_email": "firebase-adminsdk-fbsvc@whisker-media.iam.gserviceaccount.com",
-  "client_id": "109038861599775806223",
+  "client_id": "REMOVED_FOR_SECURITY_REASONS",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
